34e428528edc1ed9f2fef244f21518797bc6fc3b
Kenyasd <bejecken@gmail.com>
2025-10-19 00:51:29 +0800

    Changes

--- SQL_FINAL(Kapoy na)/LOGS.Designer.cs
+++ SQL_FINAL(Kapoy na)/LOGS.Designer.cs
@@ -236,6 +236,7 @@
             // 
             // dgvLogs
             // 
+            this.dgvLogs.AllowUserToAddRows = false;
             this.dgvLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
